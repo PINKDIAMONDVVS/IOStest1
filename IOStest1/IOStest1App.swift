c93ad4853ce5573278d0d04578c01878afad0d7d
PINKDIAMONDVVS <chenjunyi531@gmail.com>
2024-02-13 22:40:41 -0500

    Initial Commit

---
+++ IOStest1/IOStest1App.swift
@@ -0,0 +1,17 @@
+//
+//  IOStest1App.swift
+//  IOStest1
+//
+//  Created by Junyi Chen on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct IOStest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
